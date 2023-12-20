d3014434481f06f3378777f476ddfa350661d0c1
jiseub33 <jiseub33@gmail.com>
2023-12-21 02:05:20 +0900

    update

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var mySwiper = new Swiper('.swiper-container', {
-      // Add your Swiper options here
-    });
-  
     var btnNext = document.querySelector('.swiper-slide:nth-child(1) .btn_next');
   
     btnNext.addEventListener('click', function () {
